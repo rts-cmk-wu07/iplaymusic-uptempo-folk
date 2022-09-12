6df4ee8321628b1bebe532c3a34ae1e5e4b6fdd0
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-09-12 14:04:41 +0200

    add layout

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,11 +1,16 @@
 import Nav from "./Nav";
 import Search from "./Search";
 import { Outlet } from "react-router-dom";
+import Previous from "./Previous";
 
 const Layout = () => {
   return (
     <div className="h-screen">
-      <Search />
+        <div>
+            <Previous/>
+           <Search /> 
+        </div>
+      
       <main className="border-black-600 border-2 min-h-[90%]">
         <Outlet />
       </main>
