2c6201c55a1cfe911beef724467044c0d9d68ecd
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-09-12 14:05:18 +0200

    Add navlinks + icons

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -1,26 +1,38 @@
 import { Link, NavLink } from "react-router-dom";
-import { IoAlbumsSharp } from "react-icons/io";
+import { IoAlbumsSharp } from "react-icons/io5";
+import { RiPlayListFill } from "react-icons/ri";
+import { MdOutlineWifiTethering } from "react-icons/md";
+import { VscColorMode } from "react-icons/vsc";
+import { BiCategory } from "react-icons/bi";
 
 const Nav = () => {
   return (
     <nav className="flex justify-end">
-      <ul className="flex m-auto justify-between">
+      <ul className="flex m-auto justify-between w-4/5 mt-4">
         <li>
           <NavLink to="/albums">
-            <IoAlbumsSharp />
+            <IoAlbumsSharp size={35} />
           </NavLink>
         </li>
         <li>
-          <NavLink to="/playlists">Playlists</NavLink>
+          <NavLink to="/playlists">
+            <RiPlayListFill size={35} />
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/">Featured</NavLink>
+          <NavLink to="/">
+            <MdOutlineWifiTethering size={35} />
+          </NavLink>
         </li>
         <li>
-          <button>Color themes</button>
+          <button>
+            <VscColorMode size={35} />
+          </button>
         </li>
         <li>
-          <NavLink to="/categories">Categories</NavLink>
+          <NavLink to="/categories">
+            <BiCategory size={35} />
+          </NavLink>
         </li>
       </ul>
     </nav>
