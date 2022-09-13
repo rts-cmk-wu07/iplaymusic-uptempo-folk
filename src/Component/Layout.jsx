f44cdd6e6ca27ff5141cdf918efe2f82966132e8
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-09-13 14:21:45 +0200

    add player comp til layout

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -2,24 +2,23 @@ import Nav from "./Nav";
 import Search from "./Search";
 import { Outlet } from "react-router-dom";
 import Previous from "./Previous";
-
+import Player from "../Pages/Player";
 
 const Layout = () => {
   return (
     <div className="h-screen">
-        <div>
-            <Previous/>
-           <Search /> 
-        </div>  
-      <main className="border-black-600 border-2 min-h-[90%] dark:bg-slate-800">
+      <div>
+        <Previous />
+        <Search />
+      </div>
+      <main className="border-black-600 border-2 min-h-[87%] dark:bg-slate-800">
         <Outlet />
       </main>
+      <Player />
 
       <Nav />
     </div>
   );
 };
-
-
 
 export default Layout;
