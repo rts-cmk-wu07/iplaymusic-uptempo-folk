0de27fb193b01636c5d572fb8452b40a534e1e5f
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-09-14 14:21:13 +0200

    add trial

--- src/Pages/Player.jsx
+++ src/Pages/Player.jsx
@@ -1,32 +1,49 @@
 import { useState } from "react";
-import playerBg from "../assets/player-bg.jpg";
+import playerBg from "../assets/player-bgr.jpg";
+import playerFg from "../assets/player-fg.jpg";
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import audioFile from "../assets/penguin.mp3";
+import { IoChevronBack } from "react-icons/io5";
+import MaximizedAudioPlayer from "../Component/MaximizedAudioPlayer";
+import MinimizedAudioPlayer from "../Component/MinimizedAudioPlayer";
 
 const Player = () => {
   const [maximized, setMaximized] = useState(true);
+
+  const artistName = "Willie Nelson";
+  const songTitle = "On the Road Again";
 
   return (
     <>
       <>
         {!maximized && (
-          <div className="absolute right-2 bottom-20 w-full h-14 bg-yellow-400">
+          <div className="absolute right-2 bottom-20 w-full h-44 bg-yellow-400">
             <div className="flex">
               <div className="flex" onClick={(event) => setMaximized(true)}>
-                {" "}
-                <img src="" alt="" />{" "}
+                <div className="w-2 h-2">
+                  <img className="w-2 h-2" src={playerFg} alt="" />
+                </div>
                 <div>
-                  <p>Song title</p>
-                  <p>Artist name</p>
+                  <p>{artistName}</p>
+                  <p>{songTitle}</p>
                 </div>{" "}
               </div>
-              <h2>Playing</h2>
             </div>
+            <MinimizedAudioPlayer />
           </div>
         )}
         {maximized && (
           <div className="absolute right-0 top-0 bottom-0 w-screen bg-cyan-300">
-            <button onClick={(event) => setMaximized(false)}>
-              Du kan altid afbryde din forbindelse hér, og miste alt
-            </button>
+            <div className="flex">
+              <button onClick={(event) => setMaximized(false)}>
+                <IoChevronBack />
+              </button>
+              <h2>Playing</h2>
+            </div>
+            <div>
+              <MaximizedAudioPlayer audioFile={audioFile} />
+            </div>
           </div>
         )}
       </>
