771aa3feb41ca33e7f52a3d47a4f99cdb0b8d60b
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-09-23 10:54:59 +0200

    add tbody tag to playlist table

--- src/Pages/Playlists.jsx
+++ src/Pages/Playlists.jsx
@@ -56,47 +56,49 @@ const Playlists = () => {
         </h3>
 
         <table>
-          {FeaturedData[0].tracks.map((item) => (
-            <tr onClick={() => setSongPlaying(item.id)} className="my-11">
-              <td className="px-5 py-4">
-                {!songPlaying && (
-                  <button
-                    className="p-2 rounded-full w-8 h-8 bg-gradient-to-r from-[#FF1168] to-[#f2bc06]"
-                    onClick={(event) => {
-                      setSongPlaying(true);
-                      //pressPlay();
-                    }}
-                  >
-                    <IoPlay className="text-white" size="15" />
-                  </button>
-                )}
-                {songPlaying && (
-                  <button
-                    className="p-2 rounded-full w-8 h-8 bg-gradient-to-r from-[#FF1168] to-[#f2bc06]"
-                    onClick={(event) => {
-                      setSongPlaying(false);
-                      //pressPause();
-                    }}
-                  >
-                    <IoPause className="text-white" size="15" />
-                  </button>
-                )}
-              </td>
-              <td>
-                <p className="pb-1/2 font-bold text-lg dark:text-white">
-                  {item.title}
-                </p>
-                <p className="font-medium dark:text-white text-base">
-                  {item.artist}
-                </p>
-              </td>
-              <td>
-                <p className="mr-3 pl-20 font-medium dark:text-white text-base">
-                  {item.length}
-                </p>
-              </td>
-            </tr>
-          ))}
+          <tbody>
+            {FeaturedData[0].tracks.map((item) => (
+              <tr onClick={() => setSongPlaying(item.id)} className="my-11">
+                <td className="px-5 py-4">
+                  {!songPlaying && (
+                    <button
+                      className="p-2 rounded-full w-8 h-8 bg-gradient-to-r from-[#FF1168] to-[#f2bc06]"
+                      onClick={(event) => {
+                        setSongPlaying(true);
+                        //pressPlay();
+                      }}
+                    >
+                      <IoPlay className="text-white" size="15" />
+                    </button>
+                  )}
+                  {songPlaying && (
+                    <button
+                      className="p-2 rounded-full w-8 h-8 bg-gradient-to-r from-[#FF1168] to-[#f2bc06]"
+                      onClick={(event) => {
+                        setSongPlaying(false);
+                        //pressPause();
+                      }}
+                    >
+                      <IoPause className="text-white" size="15" />
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <p className="pb-1/2 font-bold text-lg dark:text-white">
+                    {item.title}
+                  </p>
+                  <p className="font-medium dark:text-white text-base">
+                    {item.artist}
+                  </p>
+                </td>
+                <td>
+                  <p className="mr-3 pl-20 font-medium dark:text-white text-base">
+                    {item.length}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </section>
       <section className="flex justify-center mt-12 mb-16">
