4a9224ec7272b4bc5cb9fa77af052425c3d2a1a2
hari2789 <U2405941@rts-365.dk>
2022-09-29 09:45:34 +0200

    categories

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -4,18 +4,18 @@ const Categories = () => {
     return (
         <div>
             
-            <h1 className="text-4xl font-bold bg-clip-text bg-gradient-to-r text-transparent from-[#EE0979] to-[#FF6A00] ">Categories</h1>
+            <h1 className="text-4xl font-bold bg-clip-text bg-gradient-to-r text-transparent from-[#EE0979] to-[#FF6A00] pt-20  ">Categories</h1>
             <div className="pt-5">
                 {CategoriesData.map((item) => (
                     <div className="pb-3">
                         <div className="bg-primaryColor  border-black border ">
-                            <a><button className="ml-2 text-white font-bold dark:text-black">{item.CategoryName}</button></a>
-                            <button className="align-middle ">
+                            <button className="ml-2 text-white font-bold dark:text-black">{item.CategoryName}</button>
+                            <button className=" align-middle ml-60">
                                 <IoIosMore color="white" size={40} />
                             </button>
                         </div>
-                        <div className="flex text-black dark:text-white">
-                            <a className="flex ml-10">{item.SubcategoryName}</a>
+                        <div className="text-black dark:text-white">
+                            <a>{item.SubcategoryName}</a>
                         </div>
                     </div>
                 ))}
