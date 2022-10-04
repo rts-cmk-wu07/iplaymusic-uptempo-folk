b3ecb6520c00bac25a581dac4c0b1d11c6a3ca85
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-10-04 11:37:43 +0200

    add token-attempt

--- src/App.js
+++ src/App.js
@@ -11,13 +11,46 @@ import GenericArtistGenre from "./Pages/GenericArtistGenre";
 import GenericPlaylistAlbum from "./Pages/GenericPlaylistAlbum";
 
 import Login from "./Pages/Login";
+import Callback from "./Pages/Callback";
+import TokenContext from "./Contexts/TokenContext";
+import { useState } from "react";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Featured />} />
+//             <Route path="/albumDetails" element={<AlbumDetails />} />
+//             <Route path="/albums" element={<Albums />} />
+//             <Route path="/playlists" element={<Playlists />} />
+//             <Route path="/categories" element={<Categories />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route
+//               path="/genericArtistGenre"
+//               element={<GenericArtistGenre />}
+//             />
+//             <Route
+//               path="/genericPlaylistAlbum"
+//               element={<GenericPlaylistAlbum />}
+//             />
+//           </Route>
+//           *
+//         </Routes>
+//       </>
+//     </div>
+//   );
+// }
 
 function App() {
+  var tokenState = useState(null);
+
   return (
-    <div className="App">
+    <TokenContext.Provider value={tokenState}>
       <>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          {/* <Route path="/" element={<Layout />}>
             <Route index element={<Featured />} />
             <Route path="/albumDetails" element={<AlbumDetails />} />
             <Route path="/albums" element={<Albums />} />
@@ -32,12 +65,13 @@ function App() {
               path="/genericPlaylistAlbum"
               element={<GenericPlaylistAlbum />}
             />
-          </Route>
-          *
+          </Route> */}
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/callback" element={<Callback />}></Route>
+          <Route path="/featured" element={<Featured />} />
         </Routes>
       </>
-    </div>
+    </TokenContext.Provider>
   );
 }
-/*<Route path="/" element={Login} /> This Rote should be placed below the main route component*/
 export default App;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,31 +1,60 @@
-import { useContext } from "react"
-import { Navigate } from "react-router-dom"
+//import { useContext } from "react"
+//import { Navigate } from "react-router-dom"
+
+// export default function Login() {
+
+//   return (
+//     <div className="bg-white text-secondaryColor  dark:text-white dark:bg-primaryColor">
+//       <h2 className="flex justify-start font-bold text-3xl">Log In</h2>
+//       <form className="ml-10" >
+//         <div className="p-16 mt-48">
+//         <div className="">
+//           <label >Username
+//             <input  className="pl-3 placeholder-neutral-900" placeholder="Enter your username" type="text" name="username" />
+//           </label>
+//         </div>
+//         <div className="mt-6">
+//           <label>Password
+//             <input className="pl-3  placeholder-neutral-900" placeholder="Enter your password" type="password" name="password" />
+//           </label>
+//         </div>
+//         <div className="pt-16">
+//         <button className="border rounded-md border-black p-4 m-10" type="submit">Sign in</button>
+//         </div>
+
+//         </div>
+//       </form>
+//     </div>
+//   )
+// }
+
+// import querystring from "querystring";
 
 export default function Login() {
+  // var queryParameters=querystring.stringify({
+  //   response_type:"code",
+  //   client_id:"1752a43ddabe4656a3551ab520423e3a",
+  //   scope:"user-read-private user-read-email",
+  //   redirect_url:"http://localhost:8888/callback",
+  //   state:"skdjfneahrieqjfasknf3æqi4ur398riwjefwfng03ri21fvl21345"
 
+  // });
+
+  const searchParams = new URLSearchParams();
+  searchParams.append("response_type", "code");
+  searchParams.append("client_id", "1752a43ddabe4656a3551ab520423e3a");
+  searchParams.append("scope", "user-read-private user-read-email");
+  searchParams.append("redirect_uri", "http://localhost:8888/callback");
+  searchParams.append("state", "qi4ur3980swd3ri21fvl21345");
+
+  var queryParameters = searchParams.toString();
 
   return (
-    <div className="bg-white text-secondaryColor  dark:text-white dark:bg-primaryColor">
-      <h2 className="flex justify-start font-bold text-3xl">Log In</h2>
-      <form className="ml-10" >
-        <div className="p-16 mt-48">
-        <div className="">
-          <label >Username
-            <input  className="pl-3 placeholder-neutral-900" placeholder="Enter your username" type="text" name="username" />
-          </label>
-        </div>
-        <div className="mt-6">
-          <label>Password
-            <input className="pl-3  placeholder-neutral-900" placeholder="Enter your password" type="password" name="password" />
-          </label>
-        </div>
-        <div className="pt-16">
-        <button className="border rounded-md border-black p-4 m-10" type="submit">Sign in</button>
-        </div>
-        
-        </div>
-      </form>
-    </div>
-  )
+    <>
+      <h1>Login</h1>
+      <a href={`https://accounts.spotify.com/authorize?${queryParameters}`}>
+        Log in with Spotify
+      </a>
+    </>
+  );
 }
-
