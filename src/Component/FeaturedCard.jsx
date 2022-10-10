ae0207bc0d6c68a1c83bfa0a7dde501a18162d4c
Maryam <mahta.ir@gmail.com>
2022-10-10 08:53:47 +0200

    test

--- src/Component/FeaturedCard.jsx
+++ src/Component/FeaturedCard.jsx
@@ -4,7 +4,7 @@ export default function FeaturedCard({ image, album, genre, id }) {
     const navigate = useNavigate();
 	return (
 		<article className="featuredCard" onClick={() =>navigate(`/playlists/${id}`)}>
-			<div className="featured-card m-auto rounded-md shadow-lg shadow-black">
+			<div className="featured-card m-auto rounded-md  shadow-lg shadow-black">
 				<img src={image} className=" m-auto w-[310px] h-[500px] mt-10 rounded-md shadow-lg" alt=""/>
 			<div>
 				<p className="featuredText absolute text-3xl font-extrabold text-white top-3/4 left-1/2 -translate-x-2/3 -translate-y-1/3 ">
