58b35fba4b92a743f40f8fdd08f6ce8d791faadd
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-10-11 09:53:16 +0200

    add back to naormal

--- src/Component/PlaylistHead.jsx
+++ src/Component/PlaylistHead.jsx
@@ -1,18 +1,18 @@
-const PlaylistHead = () => {
+const PlaylistHead = ( { playlist } ) => {
   return (
     <section className="relative h-96 z-0">
       <img
-        src={FeaturedData[4].linkImg}
-        alt={FeaturedData[4].title}
+        src={playlist.images[0].url}
+        alt={playlist.name}
         className="h-full object-cover"
       />
       <div className="-left-3 top-20 absolute">
         {" "}
         <h2 className="text-[48px] ml-10  leading-tight font-extrabold z-20 text-white ">
-          {FeaturedData[4].title}
+          {playlist.name}
         </h2>
         <h3 className="text-[20px] ml-10 mt-2 leading-tight font-extrabold z-20 text-white absolute">
-          {FeaturedData[4].tracks.length} songs
+          {/* {FeaturedData[4].tracks.length} songs */}
         </h3>
       </div>
     </section>
